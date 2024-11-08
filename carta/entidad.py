25abe31d80843dbc0fe957cb09babd2b8baa153d
Zase55 <sergiocobo90@hotmail.es>
2024-11-08 12:11:06 +0100

    add carta

---
+++ carta/entidad.py
@@ -0,0 +1,10 @@
+class Carta:
+    def __init__(self,nombre,tipo,elemento,multicopia,categoria,oficio,texto,poder):
+        self.nombre = nombre
+        self.tipo = tipo
+        self.elemento = elemento
+        self.multicopia = multicopia
+        self.categoria = categoria
+        self.oficio = oficio
+        self.texto = texto
+        self.poder = poder
