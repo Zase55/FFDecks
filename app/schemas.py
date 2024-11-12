656800d940bb5fee41154c99ef2dc7dfe5ee7d7c
Zase55 <sergiocobo90@hotmail.es>
2024-11-12 11:47:32 +0100

    add app schemas

---
+++ app/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, EmailStr, constr
+
+class UserBaseSchema(BaseModel):
+    username: constr(strip_whitespace=True, min_length=3) # type: ignore
+
+class UserLoginSchema(UserBaseSchema):
+    password: constr(min_length=8) # type: ignore
+
+class UserRegisterSchema(UserLoginSchema):
+    email: EmailStr
