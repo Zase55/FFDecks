9e25d87a6ec657e2ef32fe300846b42fe2ef1dfc
Zase55 <sergiocobo90@hotmail.es>
2024-11-13 15:29:30 +0100

    feat: new function check password

--- app/services.py
+++ app/services.py
@@ -1,7 +1,9 @@
 from app.models import db, User
-from werkzeug.security import generate_password_hash
-from app.schemas import UserRegisterSchema
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.schemas import UserRegisterSchema, UserLoginSchema
+from flask_jwt_extended import create_access_token
 
+#función para crear los datos de registro.
 def create_user(user_data: UserRegisterSchema):
     hashed_password = generate_password_hash(user_data.password)
     user = User(username = user_data.username,
@@ -11,3 +13,10 @@ def create_user(user_data: UserRegisterSchema):
     db.session.add(user)
     db.session.commit()
     return user
+
+#Función para comprobar el password.
+def check_password(user_login: UserLoginSchema):
+    user: User = User.query.filter_by(username = user_login.username).first()
+    if user and check_password_hash(user.password, user_login.password):
+        access_token = create_access_token(identity=user_login.username)
+        return access_token
