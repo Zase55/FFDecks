8a73c501657efbe938792b4c826466a079981842
Zase55 <sergiocobo90@hotmail.es>
2024-11-13 11:25:18 +0100

    feat: JWT

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL", "sqlite:///mydb.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "super-secret-key"
+    JWT_SECRET_KEY = "super-secret"
