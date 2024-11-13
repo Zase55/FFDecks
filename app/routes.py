8b804be91f215480e4dddba46d87e4334f912143
Zase55 <sergiocobo90@hotmail.es>
2024-11-13 11:27:58 +0100

    feat: login and protected

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template, request, Blueprint, jsonify # type: ignore
-from app.schemas import UserRegisterSchema
+from app.schemas import UserRegisterSchema, UserLoginSchema
+from app.models import User
 from app.services import create_user
 from pydantic import ValidationError
+from werkzeug.security import check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 cards = []
 
@@ -94,3 +97,26 @@ def register():
         return jsonify(e.errors()), 400
     user = create_user(user_register)
     return jsonify({"message": "Usuario creado correctamente.", "user_id": user.id}), 201
+
+@api.route('/login', methods=['POST'])
+def login():
+    try:
+        user_login = UserLoginSchema(**request.get_json())
+    except ValidationError as e:
+        return jsonify(e.errors()), 400
+    user: User = User.query.filter_by(username = user_login.username).first()
+    if user and check_password_hash(user.password, user_login.password):
+        access_token = create_access_token(identity=user_login.username)
+        return jsonify({"message": "Usuario logueado correctamente.", "access_token": access_token}), 200
+
+        #return jsonify({"message": "Usuario logueado correctamente."}), 200
+    return jsonify({"message": "Error, la contraseña es errónea."}), 401
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
