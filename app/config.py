247ecc145ac3179cc69acf57a8dd4688bbd962e3
Zase55 <sergiocobo90@hotmail.es>
2024-11-15 11:36:17 +0100

    feat: new JWT options

--- app/config.py
+++ app/config.py
@@ -5,3 +5,8 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "super-secret-key"
     JWT_SECRET_KEY = "super-secret"
+    JWT_TOKEN_LOCATION = ["cookies","headers"]
+    JWT_COOKIE_SECURE = False
+    JWT_ACCESS_COOKIE_NAME = "access_token"
+    JWT_COOKIE_CSRF_PROTECT = False
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
