581f8f6c4ecae7ca08a039b8f5d769f1a8d926ea
Zase55 <sergiocobo90@hotmail.es>
2024-11-18 11:44:22 +0100

    refactor: change database url

--- app/config.py
+++ app/config.py
@@ -1,11 +1,14 @@
 from os import environ
 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL", "sqlite:///mydb.sqlite3")
+    SQLALCHEMY_DATABASE_URI = environ.get(
+        "DATABASE_URL", "postgresql+psycopg2://admin:1234@localhost:5432/root"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "super-secret-key"
     JWT_SECRET_KEY = "super-secret"
-    JWT_TOKEN_LOCATION = ["cookies","headers"]
+    JWT_TOKEN_LOCATION = ["cookies", "headers"]
     JWT_COOKIE_SECURE = False
     JWT_ACCESS_COOKIE_NAME = "access_token"
     JWT_COOKIE_CSRF_PROTECT = False
