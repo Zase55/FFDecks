a1145f1bb72e039479110b47323c695944a4992c
Zase55 <sergiocobo90@hotmail.es>
2024-11-20 12:04:39 +0100

    fix: password length

--- app/models.py
+++ app/models.py
@@ -2,10 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     email = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(800), nullable=False)
