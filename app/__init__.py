ba5b65db9722fc58f43d567bdab2321790a0212b
Zase55 <sergiocobo90@hotmail.es>
2024-12-02 11:46:28 +0100

    chore: change location templates and static

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from app.routes import blueprints
 
 
 def create_app():
-    app = Flask(__name__, template_folder="./../templates", static_folder="./../static")
+    app = Flask(__name__, template_folder="./templates", static_folder="./static")
     app.config.from_object(Config)
     db.init_app(app)
     Migrate(app, db)
