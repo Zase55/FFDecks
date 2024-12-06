54f64ba582ee1c785df4e493949d5f0e09bef65a
Zase55 <sergiocobo90@hotmail.es>
2024-12-06 15:10:48 +0100

    feat: add celery worker

---
+++ app/celery_worker.py
@@ -0,0 +1,17 @@
+from os import environ
+
+import yagmail
+from celery import Celery
+
+MAIL_ID = environ.get("MAIL_ID")
+MAIL_PASSWORD = environ.get("MAIL_PASSWORD")
+
+yag = yagmail.SMTP(user=MAIL_ID, password=MAIL_PASSWORD)
+
+redis_broker = f"redis://{environ.get('REDIS_HOST')}:6379"
+celery_app = Celery("send email", broker=redis_broker, backend=redis_broker)
+
+
+@celery_app.task
+def send_without_attachment(recipient_email):
+    yag.send(to=recipient_email, subject="Testing Yagmail", contents="Hurray, it worked!")
