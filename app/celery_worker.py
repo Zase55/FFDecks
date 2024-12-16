92ea7b6e5315f9afdccfb9cec87470c2d668b558
Zase55 <sergiocobo90@hotmail.es>
2024-12-16 13:36:12 +0100

    feat: retry send email

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -12,8 +12,10 @@ redis_broker = f"redis://{environ.get('REDIS_HOST')}:6379"
 celery_app = Celery("send email", broker=redis_broker, backend=redis_broker)
 
 
-@celery_app.task
-def send_without_attachment(
-    recipient_email, subject="Prueba", message="Mensaje de Prueba."
-):
-    yag.send(to=recipient_email, subject=subject, contents=message)
+@celery_app.task(bind=True)
+def send_without_attachment(self, recipient_email, subject, message):
+    try:
+        yag.send(to=recipient_email, subject=subject, contents=message)
+        return f"Correo enviado a {recipient_email}"
+    except Exception as e:
+        self.retry(exc=e, countdown=60, max_retries=3)  # Reintentar si falla
